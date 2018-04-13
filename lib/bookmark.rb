3095f86d1c9f9e559f9dac7911d8fa9df2813b66
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-13 11:59:22 +0100

    links with hyperlinks, tests failing

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,6 +1,13 @@
 require 'pg'
 
 class Bookmark
+
+  attr_reader :url 
+
+  def initialize(url)
+    @url = url
+  end
+
   def self.connection
     if ENV['RACK_ENV'] == 'test'
       return PG.connect(dbname: 'bookmark_manager_test')
@@ -11,12 +18,13 @@ class Bookmark
 
   def self.all
     result = connection.exec("SELECT * FROM bookmarks")
-    result.map { |link| link['url'] }
+    result.map { |bookmark| Bookmark.new(bookmark['url']) }
   end
 
   def self.create(options)
   	if valid_url?(options)
-	  connection.exec("INSERT INTO bookmarks (url, name) VALUES('#{options[:url]}', '#{options[:name]}')")
+	  result = connection.exec("INSERT INTO bookmarks (url, name) VALUES('#{options[:url]}', '#{options[:name]}') RETURNING id, url")
+    Bookmark.new(result.first['id'], result.first['url'])
 	    true
   	else
   	  false
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,33 +3,30 @@ require 'pry'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all bookmarks in an array' do
+    it 'returns all bookmarks wrapped in an instance' do
       # connection = PG.connect(dbname: 'bookmark_manager_test')
+      bookmarks = Bookmark.all
+      urls = bookmarks.map(&:url)
 
       # Add the test data
-      Bookmark.create(url: 'http://databasetest1.com', name: 'databasetest1' )
-      Bookmark.create(url: 'http://databasetest2.com', name: 'databasetest2' )
-
-      expected_bookmarks = [
-        'http://databasetest1.com',
-        'http://databasetest2.com'
-      ]
-
-      expect(Bookmark.all).to eq expected_bookmarks
+      expect(urls).to include(url: 'http://databasetest1.com', name: 'databasetest1' )
+      expect(urls).to include(url: 'http://databasetest2.com', name: 'databasetest2' )
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://www.testbookmark.com', name: 'testbookmark')
+      bookmarks = Bookmark.all
+      urls = bookmarks.map(&:url)
 
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+      expect(urls).to include (url: 'http://www.testbookmark.com', name: 'testbookmark')
     end
 
     it 'not creating a bookmark if url is not valid' do
-      Bookmark.create(url: 'not a valid url')
+      bookmarks = Bookmark.all
+      urls = bookmarks.map(&:url)
 
-      expect(Bookmark.all).not_to include 'not a valid url'
+      expect(urls).not_to include 'not a valid url'
     end
   end
 end
